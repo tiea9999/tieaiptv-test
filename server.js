d196bb8be92d52c492104604190932bac86c6445
tiea9999 <155359022+tiea9999@users.noreply.github.com>
2026-03-01 17:54:50 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,23 +5,23 @@ import cors from "cors";
 const app = express();
 app.use(cors());
 
-// ===== CONFIG =====
+// CONFIG
 const HOST = "http://safetv.vip:8080";
 const USER = "8gULQeqH3I";
 const PASS = "kbuahRdUJV";
 
-// ===== CHANNEL =====
+// CHANNEL
 const channels = {
-  mono2: { id: "130725", name: "MONOMAX2" },
-  mono1: { id: "130734", name: "MONOMAX1" }
+  mono2: { id: "130724", name: "MONOMAX2" },
+  mono1: { id: "130725", name: "MONOMAX1" }
 };
 
-// ===== HOME =====
+// HOME
 app.get("/", (req,res)=>{
-res.send("TIEA IPTV HLS PROXY RUNNING");
+res.send("TIEA IPTV STREAM PROXY RUNNING");
 });
 
-// ===== PLAYLIST =====
+// PLAYLIST
 app.get("/playlist.m3u",(req,res)=>{
 
 let m3u="#EXTM3U\n";
@@ -40,49 +40,14 @@ res.send(m3u);
 
 });
 
-// ===== M3U8 =====
+// STREAM
 app.get("/:name", async (req,res)=>{
 
 const ch = channels[req.params.name];
 
 if(!ch) return res.status(404).send("Channel not found");
 
-const url = `${HOST}/live/${USER}/${PASS}/${ch.id}.m3u8`;
-
-try{
-
-const r = await fetch(url,{
-headers:{
-"User-Agent":"Mozilla/5.0"
-}
-});
-
-let text = await r.text();
-
-// rewrite segment
-text = text.replace(
-/(.*\.ts)/g,
-`/segment/${ch.id}/$1`
-);
-
-res.setHeader("Content-Type","application/vnd.apple.mpegurl");
-res.send(text);
-
-}catch(e){
-
-res.status(500).send(e.message);
-
-}
-
-});
-
-// ===== SEGMENT =====
-app.get("/segment/:id/:file", async (req,res)=>{
-
-const id = req.params.id;
-const file = req.params.file;
-
-const url = `${HOST}/live/${USER}/${PASS}/${id}/${file}`;
+const url = `${HOST}/live/${USER}/${PASS}/${ch.id}`;
 
 try{
 
@@ -105,7 +70,7 @@ res.status(500).send(e.message);
 
 });
 
-// ===== START =====
+// START
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT,()=>{
