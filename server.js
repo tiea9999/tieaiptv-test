dc6f7184baa5477c97089b7705cce2735629217d
tiea9999 <155359022+tiea9999@users.noreply.github.com>
2026-03-01 15:36:59 +0700

    Update server.js

--- server.js
+++ server.js
@@ -41,8 +41,8 @@ res.status(500).send(e.message);
 
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT,()=>{
+app.listen(PORT,"0.0.0.0",()=>{
 console.log("TIEA IPTV Proxy running on "+PORT);
 });
